a4a4b0ec7dadcfe239d9317852bf867da68025a2
Bishal jaiswal <bishaljaiswal0408@gmail.com>
2025-06-28 15:12:53 +0545

    fixes deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,8 +14,8 @@ const __dirname = path.dirname(__filename)
 const app = express()
 const SERVER_PORT = process.env.PORT || 5000
 
-app.use(express.json({ limit: '100mb' }))
-app.use(express.urlencoded({ extended: true, limit: '100mb' }))
+app.use(express.json({ limit: '500mb' }))
+app.use(express.urlencoded({ extended: true, limit: '500mb' }))
 app.use(cookieParser())
 app.use(cors({
     origin: process.env.CLIENT_URL || 'http://localhost:5173',
