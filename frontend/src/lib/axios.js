5005b535d2f397f351f8051362d5544d049d0fa5
Bishal jaiswal <bishaljaiswal0408@gmail.com>
2025-06-28 14:31:16 +0545

    fixed deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === 'development'
-      ? 'http://192.168.1.4:3000/api'
+      ? 'http://192.168.1.3:3000/api'
       : '/api',
   withCredentials: true,
 });
