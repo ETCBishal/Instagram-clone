0d1ef96ff42281269e5605e286ebf9596cccad71
Bishal jaiswal <bishaljaiswal0408@gmail.com>
2025-06-28 14:38:44 +0545

    fixed deploy 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,32 +6,34 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { connectDb } from '../lib/db.js'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
-const SERVER_PORT = process.env.PORT
+const SERVER_PORT = process.env.PORT || 5000
 
 app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://192.168.1.4:5173',
-    credentials:true
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true
 }))
 
-app.use('/api/auth',authRoute)
-app.use('/api/post',postRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/post', postRoute)
 
-if(process.env.NODE_MODE !== 'development'){
-    app.use(express.static(path.join(__dirname,"..frontend/dist")))
+if (process.env.NODE_MODE !== 'development') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"..frontend","dist","index.html"))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
     })
-    
-
 }
 
-app.listen(SERVER_PORT,()=>{
-    console.log(`Server is running at the PORT: ${SERVER_PORT}`)
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running at PORT: ${SERVER_PORT}`)
     connectDb()
 })
